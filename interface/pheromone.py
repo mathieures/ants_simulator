84240859882f2ebb8dc076c2179e16682ec78cea
mathieures <mathieuresseguier@gmail.com>
2021-05-18 16:30:03 +0200

    Changed tints to a class attribute

--- interface/pheromone.py
+++ interface/pheromone.py
@@ -5,13 +5,14 @@ class Pheromone(CustomObject):
 	Classe représentant une pheromone graphiquement (un carre)
 	à des coordonnées précises, et d'une taille et couleur données
 	"""
+	
+	# Teintes roses du clair au fonce
+	tints = ["#FFDFDB", "#FBBFB8", "#F79F95", "#F38071", "#F0604D"]
 
 	def __init__(self, canvas, coords):
 		"""Instancie une pheromone graphiquement"""
 		super().__init__(canvas, coords, size=3, color="#FFDFDB")
 
-		# Teintes roses du clair au fonce
-		self._tints = ["#FFDFDB", "#FBBFB8", "#F79F95", "#F38071", "#F0604D"]
 		self._curr_tint = 0 # Index de la teinte qui correspond a la couleur courante
 
 	def draw(self):
@@ -25,5 +26,5 @@ class Pheromone(CustomObject):
 	def darken(self):
 		if self._curr_tint < 4:
 			self._curr_tint += 1
-			self._color = self._tints[self._curr_tint]
+			self._color = Pheromone.tints[self._curr_tint]
 			self._canvas.itemconfig(self._id, fill=self._color)
